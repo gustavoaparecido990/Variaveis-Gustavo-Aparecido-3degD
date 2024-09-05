ae293f8f988c05b255ba95e3656c35f1e654cdf1
gustavoaparecido990 <164099470+gustavoaparecido990@users.noreply.github.com>
2024-09-05 02:23:49 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+let anoAtual = 2024;
+const nomeCompleto = "Gustavo Aparecido";
+let idade = 17;
+const cursoFaculdade = "Advogado";
+let cidadeAtual = "SJP";
+
+console.log(`Olá, eu sou a ${nomeCompleto} eu tenho $`);
+
+anoAtual = 2025;
+
+console.log(anoAtual);
